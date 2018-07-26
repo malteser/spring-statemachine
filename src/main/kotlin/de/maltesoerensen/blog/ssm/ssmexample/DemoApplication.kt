3558b3a6017f0c2a6694bd22805b6e416cdaafb4
Malte Sörensen <maltesoerensen@gmx.net>
2018-07-26 13:28:00 +0200

    Initial

---
+++ src/main/kotlin/de/maltesoerensen/blog/ssm/ssmexample/DemoApplication.kt
@@ -0,0 +1,11 @@
+package de.maltesoerensen.blog.ssm.ssmexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
